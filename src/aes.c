43cdcba152594f9ff8b3c0086cbd7d0ea2c11090
khm <khm@khm-debian.Home>
2013-08-27 20:26:23 -0400

    Refactored the key expansion function

--- src/aes.c
+++ src/aes.c
@@ -7,59 +7,31 @@ static void RotWord( uchar *w );
 static void Rotate( uchar *w, int n );
 static uchar SubBytes( uchar x );
 static void  KeyExpansion( uchar *key, uchar *w, int Nk );
+static int set_Nr( int Nk );
+static void initial_expansion( uchar *key, uchar *w, int Nk );
+static void expand_key( uchar *key, uchar *w, int Nk, int Nr, int Nb );
  
 
 /* Internal functions */
-uchar SubBytes( uchar x )
+static uchar SubBytes( uchar x )
 {
     return 0;
 }
 
 static void  KeyExpansion( uchar *key, uchar *w, int Nk )
 {
-    uchar tmp[4] = {0};
     int i, Nb, Nr;
 
     /* Set length dependent values */
     Nb = 4;
-    switch(Nk)
-    {
-    case 16:
-        Nr = 10;
-        break;
-    case 24:
-        Nr = 12;
-        break;
-    case 32:
-        Nr = 14;
-        break;
-    }
+    Nr = set_Nr(Nk);
 
-    /* Expand initial part of the key */
-    for ( i=0; i<Nk; ++i )
-    {
-        w[4*i+0] = key[4*i+0];
-        w[4*i+1] = key[4*i+1];
-        w[4*i+2] = key[4*i+2];
-        w[4*i+3] = key[4*i+3];
-    }
+    /* Initialize key expansion */
+    initial_expansion(key,w,Nk);
+
     /* Expand the rest of the key */
-    for ( i=Nk; i<Nb*(Nr+1); ++i )
-    {
-        tmp[0] = w[4*i-4];  
-	tmp[1] = w[4*i-3];  
-	tmp[2] = w[4*i-2];  
-	tmp[3] = w[4*i-1]; 
-        if ( 0==(i%Nk) )
-            SubRotRcon(tmp,i/Nk);
-        else if ( (Nk>6) && (4==(i%Nk)) )
-            SubWord(tmp);
+    expand_key(key,w,Nk,Nr,Nb);
 
-        w[4*i+0] = w[4*(i-Nk)+0] ^ tmp[0];
-        w[4*i+1] = w[4*(i-Nk)+1] ^ tmp[1];
-        w[4*i+2] = w[4*(i-Nk)+2] ^ tmp[2];
-        w[4*i+3] = w[4*(i-Nk)+3] ^ tmp[3];
-    }
     return;
 }
 
@@ -72,7 +44,10 @@ static void SubRotRcon( uchar *w, int k )
 
 static void SubWord( uchar *w )
 {
-    w[0] = SubBytes(w[0]); w[1] = SubBytes(w[1]); w[2] = SubBytes(w[2]); w[3] = SubBytes(w[3]); 
+    w[0] = SubBytes(w[0]); 
+    w[1] = SubBytes(w[1]); 
+    w[2] = SubBytes(w[2]); 
+    w[3] = SubBytes(w[3]); 
 }
 
 static void RotWord( uchar *w )
@@ -90,5 +65,52 @@ static void Rotate( uchar *w, int n )
         u[(i+n)%4] = w[i];
     }
     memcpy(w,u,4);
+}
+
+static int set_Nr( int Nk )
+{
+    if (16==Nk)
+        return 10;
+    if (24==Nk)
+        return 12;
+    if (32==Nk)
+        return 14;
+    return -1;
+}
+
+static void initial_expansion( uchar *key, uchar *w, int Nk )
+{
+    int i;
+    for ( i=0; i<Nk; ++i )
+    {
+        w[4*i+0] = key[4*i+0]
+        w[4*i+1] = key[4*i+1]
+        w[4*i+2] = key[4*i+2]
+        w[4*i+3] = key[4*i+3]
+    }
+    return;
+}
+
+static void expand_key( uchar *key, uchar *w, int Nk, int Nr, int Nb )
+{
+    uchar tmp[4]={0};
+    int i;
+    for ( i=Nk; i<Nb*(Nr+1); ++i )
+    {
+        tmp[0] = w[4*i-4];  
+	tmp[1] = w[4*i-3];  
+	tmp[2] = w[4*i-2];  
+	tmp[3] = w[4*i-1]; 
+        if ( 0==(i%Nk) )
+            SubRotRcon(tmp,i/Nk);
+        else if ( (Nk>6) && (4==(i%Nk)) )
+            SubWord(tmp);
+
+        w[4*i+0] = w[4*(i-Nk)+0] ^ tmp[0];
+        w[4*i+1] = w[4*(i-Nk)+1] ^ tmp[1];
+        w[4*i+2] = w[4*(i-Nk)+2] ^ tmp[2];
+        w[4*i+3] = w[4*(i-Nk)+3] ^ tmp[3];
+    }
+    return;
 }
 
